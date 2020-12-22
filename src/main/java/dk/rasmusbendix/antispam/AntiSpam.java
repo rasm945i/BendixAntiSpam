5e10d78f4a5151a0a35078201b18b96f4e2d2ffd
rasm945i <rasmusb8b@gmail.com>
2020-12-22 02:45:16 +0100

    Made chat-listener variable somewhat accessible so values can be tweaked by a third-party plugin

--- src/main/java/dk/rasmusbendix/antispam/AntiSpam.java
+++ src/main/java/dk/rasmusbendix/antispam/AntiSpam.java
@@ -1,12 +1,15 @@
 package dk.rasmusbendix.antispam;
 
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AntiSpam extends JavaPlugin {
 
+    @Getter ChatListener chatListener;
+
     @Override
     public void onEnable() {
-        ChatListener listener = new ChatListener(this);
+        chatListener = new ChatListener(this);
     }
 
     @Override
